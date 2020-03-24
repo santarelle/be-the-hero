e71b33c68a8dcec12df599b9e8ff258b923df65f
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2020-03-24 16:48:35 -0400

    Add ONG Model

---
+++ backend/src/database/models/ong.model.ts
@@ -0,0 +1,8 @@
+export default class Ong {
+    id: string | undefined;
+    name: string | undefined;
+    email: string | undefined;
+    whatsapp: string | undefined;
+    city: string | undefined;
+    uf: string | undefined;
+}
